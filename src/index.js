923ce1008f60714356d2d5863c4d86d9cfc32a05
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-05-19 00:26:28 +0300

    logging errors using Sentry.io

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://fe5d6bafd63c4f2a9737b04d4358aff1@o1252868.ingest.sentry.io/6419187",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
